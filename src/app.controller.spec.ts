54d821d1856533a767de5fb9275c55ead691ab90
ibrahimshamma99 <i.abushammah@gmail.com>
2020-01-31 15:29:36 +0200

    decorator environment explaination

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {} from './playground/cats.controller';
+import { cat } from './app.controller';
 
 describe('AppController', () => {
   let appController: AppController;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+/**SECTION 
+ * The routing mechanism controls which controller receives which requests. 
+ * Frequently, each controller has more than one route, 
+ * and different routes can perform different actions.
+ */
+import { Controller, Get, Post , Redirect , Query} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
+  
+  @Get("/Ibrahim")
+  @Redirect('https://github.com/IbrahimShamma99/Nest-Starter', 301)
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+    getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
