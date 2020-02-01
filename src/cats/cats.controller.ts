188fcdd55ede3c0b51d84aa41af6306729e0a2d5
ibrahimshamma99 <i.abushammah@gmail.com>
2020-02-01 11:54:43 +0200

    provider

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,21 @@
-import { Controller , Get , Redirect} from '@nestjs/common';
+import { Controller, Get, Req , Res ,Body , Redirect , HttpCode , Post } from '@nestjs/common';
+import { Request } from 'express';
+//NOTE TO create your own decorators import { createParamDecorator } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
-    
-    @Get()
-    // @Redirect("https://www.petsworld.in/blog/cat-pictures-funny-cute-adorable-and-all-time-favorite-cat-images.html")
-    findAll(): string {
-      return 'This action returns all cats';
-    }
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
 }
