f1f239d3e5ffcd8c4e6fabf10d7e5cd5b2df2964
ibrahimshamma99 <i.abushammah@gmail.com>
2020-02-04 10:47:51 +0200

    Update DOCS

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
 
+//SECTION 
+/*service => 
+  Injectable marks CatsService as provider
+  Then we request that Nest inject the provider into our controller class.
+  Finally, we register the provider with the Nest IoC container in app modules
+  NOTE IOC => Inversion of control
+  */
+
 @Injectable()
 export class CatsService {
     private readonly cats: Cat[] = [];
--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,22 @@ const server = async function () {
   await app.listen(3000);
 }
 server();
+
+/**ANCHOR 
+ * Nest is a platform-agnostic framework. 
+ * This means you can develop reusable logical parts that can be 
+ * used across different types of applications. For example, 
+ * most components can be re-used without change across different 
+ * underlying HTTP server frameworks (e.g., Express and Fastify), 
+ * and even across different types of applications 
+ * (e.g., HTTP server frameworks, Microservices with different transport layers, and Web Sockets).
+ */
+
+ /**NOTE 
+  * We can divide the overall lifecycle into three phases: 
+  * initializing, running and terminating. 
+  * Using this lifecycle, you can plan for appropriate 
+  * initialization of modules and services, manage active 
+  * connections, and gracefully shutdown your application 
+  * when it receives a termination signal.
+  */
