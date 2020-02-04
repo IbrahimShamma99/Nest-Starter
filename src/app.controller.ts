7cf6553db2c3650a9c1b9958fb09f105df780e74
ibrahimshamma99 <i.abushammah@gmail.com>
2020-02-04 14:54:43 +0200

    login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,21 +3,28 @@
  * Frequently, each controller has more than one route, 
  * and different routes can perform different actions.
  */
-import { Controller, Get, Post , Redirect , Query} from '@nestjs/common';
+import { Controller, Get, Post , Redirect , Query , Request, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   
-  // @Get("/Ibrahim")
-  // @Redirect('https://github.com/IbrahimShamma99/Nest-Starter', 301)
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-  
-  @Get('/')
-  @Redirect("http://localhost:3000/")
+  @Get("/Ibrahim")
+  @Redirect('https://github.com/IbrahimShamma99/Nest-Starter', 301)
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;}
+
+
+  @Get('/NIGGAA')
+  @Redirect("https://docs.nestjs.com/v5/")
 
   @Get('/docs')
   @Redirect('https://docs.nestjs.com', 302)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import {UsersService} from '../users/users.service';
 @Injectable()
 export class AuthService {
     constructor(private readonly userService:UsersService){}
-    async validation(username:string,pass:string): Promise<any> {
+    async validateUser(username:string,pass:string): Promise<any> {
         const user = await this.userService.findOne(username);
         if (user && user.password === pass){
             const {password , ...result} = user;
